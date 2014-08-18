41d7de737a781b53a7e9152b49ff9e59569379df
aavilavi <kouraji@hotmail.com>
2014-08-17 20:33:32 -0400

    Update SecretMission.java

--- SecretMission.java
+++ SecretMission.java
@@ -1,5 +1,7 @@
 /*
- *You are given a function 'secret()' that accepts a single integer parameter and returns an integer. In a language of your choice, write an application that determines if this function is an additive function [ secret(x+y) = secret(x) + secret(y) ] for all prime numbers under N where N is a given integer.
+ *You are given a function 'secret()' that accepts a single integer parameter and returns an integer. 
+ *In a language of your choice, write an application that determines if this function is an additive 
+ *function [ secret(x+y) = secret(x) + secret(y) ] for all prime numbers under N where N is a given integer.
  */
 
 import java.util.Arrays;
